a47b03def3fa00d8e702a3137842785d2975edb5
yawarpwn <john.kazz@gmail.com>
2023-05-14 19:01:34 -0500

    swiper fix

---
+++ out/_astro/hoisted.9a234298.js
@@ -0,0 +1,17 @@
+import"./product-card.astro_astro_type_script_index_0_lang.e4914c89.js";const e=document.querySelector("swiper-container"),t={injectStyles:[`
+          :host {
+            --swiper-theme-color: var(--theme-accent-secondary);
+            --swiper-navigation-size: 20px;
+          }
+
+          .swiper-button-next,
+          .swiper-button-prev {
+             border: 1px solid var(--theme-accent-secondary); 
+             background: var(--theme-bg); 
+             width: 40px ; 
+             height: 40px ; 
+             border-radius: 100%; 
+          }
+          .swiper-pagination-bullet {
+          }
+      `]};Object.assign(e,t);e.initialize();
