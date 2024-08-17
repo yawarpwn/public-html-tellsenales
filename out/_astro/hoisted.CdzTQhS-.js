4fe79338310a0bb60b0774cd3d240b8954b7bc29
yawarpwn <john.kazz@gmail.com>
2024-08-17 14:11:34 -0500

    update page

---
+++ out/_astro/hoisted.CdzTQhS-.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./hoisted.CRYjoTCv.js";import{P as i}from"./photoswipe-lightbox.esm.Dmq7wy8N.js";import"./hoisted.h9FVFDta.js";const r=new i({gallery:"#my-gallery",children:"a",pswpModule:()=>o(()=>import("./photoswipe.esm.CKijkUPa.js"),[])});r.init();
