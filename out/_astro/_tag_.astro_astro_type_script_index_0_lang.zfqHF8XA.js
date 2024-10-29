ea579ab7acdc79905fa5eb5bcf75197c7cc2a3a5
yawarpwn <john.kazz@gmail.com>
2024-10-28 19:42:09 -0500

    fix .htaccess

--- out/_astro/_tag_.astro_astro_type_script_index_0_lang.zfqHF8XA.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./preload-helper.CLcXU_4U.js";import{P as i}from"./photoswipe-lightbox.esm.Dmq7wy8N.js";const r=new i({gallery:"#my-gallery",children:"a",pswpModule:()=>o(()=>import("./photoswipe.esm.CKijkUPa.js"),[])});r.init();
