556a84b6792b1d7950e5590a96fd5832590237d5
johneyder <john.kazz@gmail.com>
2025-04-09 11:27:36 -0500

    add structuredData

--- out/_astro/index.D3IKQ_MP.js
+++
@@ -1,1 +0,0 @@
-const s={IndexName:"TELL_SEGURIDAD_PRODUCTS",AppId:"EVPE0RYU8N"},e="https://api.tellsignals.workers.dev/send-mail",a="https://api.tellsignals.workers.dev/subscribe";export{s as A,a as S,e as a};
